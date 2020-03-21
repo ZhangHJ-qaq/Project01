0392014ad58297c4edf8d3475ebc63696a89477e
Zhang Haojie <19302010021@fudan.edu.cn>
2020-03-22 00:19:30 +0800

    重写了上传图片的js脚本

--- src/js/jsForPhotoUpload.js
+++ src/js/jsForPhotoUpload.js
@@ -1,64 +1,54 @@
 $(function () {
-    // 上传图片，并调整图片使之成为150*150（通过加border）
-    $("#fileUploadBtn").change(function(e) {
-        let imgBox = e.target;
-        uploadImg($('#imgBox'), imgBox)
-    });
-
-    function uploadImg(element, tag) {
-        let file = tag.files[0];
-        let imgSrc;
-        if (!/image\/\w+/.test(file.type)) {
-            alert("你上传的好像不是图片");
+    jQuery("#fileUpload").change(function (e) {
+        let imgBox = jQuery("#imgBox");
+        let file = document.getElementById("fileUpload").files[0];
+        let extension = file.name.split(".")[file.name.split(".").length - 1].toLowerCase();
+        if (extension !== 'png' && extension !== 'jpg' && extension !== 'gif' && extension !== 'bmp') {
+            alert("你上传的文件不符合要求，只支持png,jpg,gif,bmp格式的图片");
             return false;
         }
-        let reader = new FileReader();
-        reader.readAsDataURL(file);
-        reader.onload = function() {
-            element.empty();
-            imgSrc = this.result;
+        let fileReader = new FileReader();
+        fileReader.readAsDataURL(file);
+        jQuery(fileReader).load(function () {
+            imgBox.empty();
             let img = document.createElement("img");
-            $(img).attr("src", imgSrc);
-            element.append(img);
-            img.onload=function () {
-                imageAdjust(img);//调整被上传图片的版式
+            jQuery(img).attr({
+                src: fileReader.result,
+                alt: ""
+            });
+            imgBox.append(img);
+            img.onload = function () {
+                imageAdjust(img);
             }
-
-        };
-    }
-
+        })
+    })
 });
 
 function imageAdjust(image) {
-    jQuery(image).each(function () {
-        let width = this.width;
-        let height = this.height;
-        let k = null;//k是比例系数
-        if (width < height) {//此时，图片是“瘦的”
-            k = 150 / height;
-            jQuery(this).css({
-                "width":width*k+"px",
-                "height":height*k+"px",
-                "border-left-width":(height-width)/2*k+"px",
-                "border-right-width":(height-width)/2*k+"px",
-                "border-style":"solid",
-                "border-color":"white"
-            })
-        }
-        else {
-            k = 150 / width;
-            jQuery(this).css({
-                "width":width*k+"px",
-                "height":height*k+"px",
-                "border-bottom-width":(width-height)/2*k+"px",
-                "border-top-width":(width-height)/2*k+"px",
-                "border-style":"solid",
-                "border-color":"white"
-            })
-        }
-    })
-
-
+    let width = image.width;
+    let height = image.height;
+    let k = null;//k是比例系数
+    if (width < height) {//此时，图片是“瘦的”
+        k = 150 / height;
+        jQuery(image).css({
+            "width": width * k + "px",
+            "height": height * k + "px",
+            "border-left-width": (height - width) / 2 * k + "px",
+            "border-right-width": (height - width) / 2 * k + "px",
+            "border-style": "solid",
+            "border-color": "white"
+        })
+    } else {
+        k = 150 / width;
+        jQuery(image).css({
+            "width": width * k + "px",
+            "height": height * k + "px",
+            "border-bottom-width": (width - height) / 2 * k + "px",
+            "border-top-width": (width - height) / 2 * k + "px",
+            "border-style": "solid",
+            "border-color": "white"
+        })
+    }
 }
 
 
